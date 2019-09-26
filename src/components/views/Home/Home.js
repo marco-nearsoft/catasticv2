e8eaeb61fd87567dde1ff77b8f6456788fcec2ce
marco-nearsoft <mmartinez@nearsoft.com>
2019-09-26 15:33:46 -0700

    Create Home view and add style

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,5 +1,25 @@
 import React from "react";
+import style from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <main css={style}>
+    <div className="image-wrapper">
+      <img
+        className="main-image"
+        src="https://cdn.thewirecutter.com/wp-content/uploads/2018/04/catbeds-2x1-05936.jpg"
+      />
+    </div>
+    <div className="buttons-wrapper">
+      <button className="reject">
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
+      <button className="accept">
+        <FontAwesomeIcon icon={faHeart} />
+      </button>
+    </div>
+  </main>
+);
 
 export default Home;
