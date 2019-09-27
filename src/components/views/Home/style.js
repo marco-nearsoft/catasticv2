95083417fe8559b4766ed1129769d305b1cc62f7
marco-nearsoft <mmartinez@nearsoft.com>
2019-09-27 11:40:21 -0700

    Add breakpoints to make it responsive on a small screen

--- src/components/views/Home/style.js
+++ src/components/views/Home/style.js
@@ -1,9 +1,14 @@
 import { css } from "@emotion/core";
 
 const style = css`
-  width: 60%;
+  width: 80%;
   margin: auto;
   text-align: center;
+  max-width: 700px;
+
+  @media (max-width: 400px) {
+    width: 90%;
+  }
 
   & .main-image {
     width: 100%;
@@ -11,6 +16,9 @@ const style = css`
   & .image-wrapper {
     padding: 2rem;
     box-shadow: 0rem 0rem 1rem 0.5rem rgba(0, 0, 0, 0.2);
+    @media (max-width: 400px) {
+      padding: 1rem;
+    }
   }
   & .buttons-wrapper {
     position: relative;
@@ -20,18 +28,24 @@ const style = css`
     font-size: 2rem;
     background-color: #fff;
     padding: 1rem;
-    border: 0.01rem solid #000;
+    border: 0.01rem solid #ccc;
     border-radius: 50rem;
     width: 6rem;
     height: 6rem;
     margin: 0 1rem;
     cursor: pointer;
-    transition: all .2s;
+    transition: all 0.2s;
+    @media (max-width: 400px) {
+      width: 5rem;
+      height: 5rem;
+      display: block;
+      margin: 1rem auto;
+    }
   }
   & .buttons-wrapper button:hover {
     color: red;
     transform: scale(1.1);
-    box-shadow: 0.5rem 0.5rem 0.5rem 0rem rgba(0,0,0,0.2);
+    box-shadow: 0.5rem 0.5rem 0.5rem 0rem rgba(0, 0, 0, 0.2);
   }
   & .buttons-wrapper .reject:hover {
     color: red;
