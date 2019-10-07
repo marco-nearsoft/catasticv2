0bdb0fed681ceb5cb500ff5f7e3334e65a7f22ba
marcomz <mmartinez@nearsoft.com>
2019-10-07 15:34:02 -0700

    Use constant for saga takeEvery

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,8 +1,9 @@
 import { takeEvery, all } from "redux-saga/effects";
 import fetchCurrentImage from "./fetchCurrentImage";
+import { START_FETCHING_CURRENT_IMAGE } from "../actions/types";
 
 function* startFetchingCurrentImage() {
-  yield takeEvery("START_FETCHING_CURRENT_IMAGE", fetchCurrentImage);
+  yield takeEvery(START_FETCHING_CURRENT_IMAGE, fetchCurrentImage);
 }
 
 export default function* rootSaga() {
