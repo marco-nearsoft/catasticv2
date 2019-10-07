d7f5882f085ab4b5e7e7aae16d9478dc0e29e556
marcomz <mmartinez@nearsoft.com>
2019-10-07 15:48:41 -0700

    Create constants for actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,15 +3,21 @@ import currentImage from "./currentImage";
 const { setCurrentImage, startFetchingCurrentImage } = currentImage;
 
 function setFavorites(payload) {
-  return { type: "SET_FAVORITES", payload };
+  return { type: SET_FAVORITES, payload };
 }
 
 function removeFromFavorites(payload) {
-  return { type: "REMOVE_FROM_FAVORITES", payload };
+  return { type: REMOVE_FROM_FAVORITES, payload };
 }
 
 function startFetchingFavorites(payload) {
-  return { type: "START_FETCHING_FAVORITES", payload };
+  return { type: START_FETCHING_FAVORITES, payload };
 }
 
-export { setCurrentImage, startFetchingCurrentImage, setFavorites, removeFromFavorites, startFetchingFavorites };
+export {
+  setCurrentImage,
+  startFetchingCurrentImage,
+  setFavorites,
+  removeFromFavorites,
+  startFetchingFavorites
+};
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,13 +1,14 @@
 import { takeEvery, all } from "redux-saga/effects";
 import fetchCurrentImage from "./fetchCurrentImage";
 import fetchFavorites from "./fetchFavorites";
+import { START_FETCHING_CURRENT_IMAGE, START_FETCHING_FAVORITES } from "../actions/types";
 
 function* startFetchingCurrentImage() {
   yield takeEvery(START_FETCHING_CURRENT_IMAGE, fetchCurrentImage);
 }
 
 function* startFetchingFavorites() {
-  yield takeEvery("START_FETCHING_FAVORITES", fetchFavorites);
+  yield takeEvery(START_FETCHING_FAVORITES, fetchFavorites);
 }
 
 export default function* rootSaga() {
