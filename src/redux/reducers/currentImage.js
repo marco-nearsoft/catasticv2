d90ce08e2b35eab338d44b8786d105e9029b81ba
marcomz <mmartinez@nearsoft.com>
2019-10-08 14:09:17 -0700

    Use action constant on reducer

--- src/redux/reducers/currentImage.js
+++ src/redux/reducers/currentImage.js
@@ -1,5 +1,7 @@
+import { SET_CURRENT_IMAGE } from "../actions/types";
+
 const currentImage = (state = {}, action) => {
-  if (action.type === "SET_CURRENT_IMAGE") {
+  if (action.type === SET_CURRENT_IMAGE) {
     return action.payload;
   }
   return state;
