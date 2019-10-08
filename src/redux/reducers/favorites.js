5c621ad491ed03a193871376550837c7ee2efe3c
marcomz <mmartinez@nearsoft.com>
2019-10-08 14:12:54 -0700

    Use action constant on reducer

--- src/redux/reducers/favorites.js
+++ src/redux/reducers/favorites.js
@@ -1,8 +1,10 @@
+import { SET_FAVORITES, REMOVE_FROM_FAVORITES } from "../actions/types";
+
 const favorites = (state = [], action) => {
-  if (action.type === "SET_FAVORITES") {
+  if (action.type === SET_FAVORITES) {
     return action.payload;
   }
-  if (action.type === "REMOVE_FROM_FAVORITES") {
+  if (action.type === REMOVE_FROM_FAVORITES) {
     return state.filter(function(value) {
       return value.id !== action.payload.id;
     });
