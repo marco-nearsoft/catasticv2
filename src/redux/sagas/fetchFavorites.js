45a6a5a40d324b20659e7bec0c73e2e2ca6d36f7
marcomz <mmartinez@nearsoft.com>
2019-10-10 12:59:18 -0700

    Use method call on axios instance

--- src/redux/sagas/fetchFavorites.js
+++ src/redux/sagas/fetchFavorites.js
@@ -1,14 +1,11 @@
-import { put } from "redux-saga/effects";
+import { put, call } from "redux-saga/effects";
 import { setFavorites } from "../actions/";
 import instance from "../../axiosInstance";
 
 function* fetchFavorites() {
-  const result = yield instance("/images/fav").then(response => {
-    return response.data;
-  }).catch(function (error) {
-    console.log(error);
-    return {images: []};
-  });
+  const response = yield call(instance.get, "/images/fav");
+
+  const result = response.data;
 
   yield put(setFavorites(result.images));
 }
