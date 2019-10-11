78d2e856a6bc0549710c1b5339882d5418fb9724
marcomz <mmartinez@nearsoft.com>
2019-10-11 13:09:15 -0700

    Avoid dispatching action when request fails on current image

--- src/redux/sagas/fetchCurrentImage.js
+++ src/redux/sagas/fetchCurrentImage.js
@@ -3,21 +3,17 @@ import { setCurrentImage } from "../actions/";
 import instance from "../../axiosInstance";
 
 function* fetchCurrentImage() {
-  let image;
   try {
     const response = yield call(instance, "/images");
 
     if (response.status === 200) {
-      image = response.data.image;
+      yield put(setCurrentImage(response.data.image));
     } else {
-      image = { id: null, url: null };
+      console.log(response);
     }
   } catch (error) {
     console.log(error);
-    image = { id: null, url: null };
   }
-
-  yield put(setCurrentImage(image));
 }
 
 export default fetchCurrentImage;
