8158b7ecbbac9d47dc4634f2f931efcf6ee206b4
marcomz <mmartinez@nearsoft.com>
2019-10-11 12:26:39 -0700

    Pass just the function to onClick

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -33,7 +33,7 @@ const ConnectedHome = props => {
         )}
       </div>
       <div className="buttons-wrapper">
-        <button className="reject" onClick={() => {props.startFetchingCurrentImage()}}>
+        <button className="reject" onClick={props.startFetchingCurrentImage}>
           <FontAwesomeIcon icon={faTimes} />
         </button>
         <button className="accept">
