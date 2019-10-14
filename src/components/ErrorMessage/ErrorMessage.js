3c48de6b3ed42a32028e89016c4b235b0492ae0e
marcomz <mmartinez@nearsoft.com>
2019-10-14 16:08:12 -0700

    Return null if there is no error message

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./style";
 
-const ErrorMessage = ({ message }) => <div css={style}>{message}</div>;
+const ErrorMessage = ({ message }) => {
+  return message ? (<div css={style}>{message}</div>) : null;
+};
 
 export default ErrorMessage;
