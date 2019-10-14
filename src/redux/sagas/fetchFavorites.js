37a24eb9d500bfdff7dfbb680e137c804d90e6ee
marcomz <mmartinez@nearsoft.com>
2019-10-14 16:04:47 -0700

    Dispatch setFavorites only if request was successful

--- src/redux/sagas/fetchFavorites.js
+++ src/redux/sagas/fetchFavorites.js
@@ -3,23 +3,18 @@ import { setFavorites, setErrorMessage } from "../actions/";
 import instance from "../../axiosInstance";
 
 function* fetchFavorites() {
-  let images;
   try {
     const response = yield call(instance.get, "/images/fav");
 
     if (response.status === 200) {
       yield put(setErrorMessage(null));
-      images = response.data.images;
+      yield put(setFavorites(response.data.images));
     } else {
       yield put(setErrorMessage("Couldn't load favorites"));
-      images = [];
     }
   } catch (error) {
     yield put(setErrorMessage("Connectivity problems"));
-    images = [];
   }
-
-  yield put(setFavorites(images));
 }
 
 export default fetchFavorites;
