815c18f641db735843e38f33d8bb774e9bea3cfb
marcomz <mmartinez@nearsoft.com>
2019-10-14 16:29:24 -0700

    Prevent post to server is there is no id

--- src/redux/sagas/likeImageOnServer.js
+++ src/redux/sagas/likeImageOnServer.js
@@ -3,22 +3,24 @@ import { addImageToFavorites, setErrorMessage } from "../actions/";
 import instance from "../../axiosInstance";
 
 function* fetchFavorites(action) {
-  try {
-    const response = yield call(
-      instance.post,
-      `/images/fav/${action.payload.id}`
-    );
+  if (action.payload.id) {
+    try {
+      const response = yield call(
+        instance.post,
+        `/images/fav/${action.payload.id}`
+      );
 
-    if (response.status === 200) {
-      yield put(setErrorMessage(null));
-      yield put(addImageToFavorites(response.data.image));
-    } else {
-      yield put(setErrorMessage("Couldn't mark image as liked"));
-      console.log(response);
+      if (response.status === 200) {
+        yield put(setErrorMessage(null));
+        yield put(addImageToFavorites(response.data.image));
+      } else {
+        yield put(setErrorMessage("Couldn't mark image as liked"));
+        console.log(response);
+      }
+    } catch (error) {
+      yield put(setErrorMessage("Connectivity problems"));
+      console.log(error);
     }
-  } catch (error) {
-    yield put(setErrorMessage("Connectivity problems"));
-    console.log(error);
   }
 }
 
--- src/redux/sagas/removeImageFromFavorites.js
+++ src/redux/sagas/removeImageFromFavorites.js
@@ -3,21 +3,23 @@ import { removeFromFavorites, setErrorMessage } from "../actions/";
 import instance from "../../axiosInstance";
 
 function* removeImageFromFavorites(action) {
-  try {
-    const response = yield call(
-      instance.delete,
-      `/images/fav/${action.payload.id}`
-    );
+  if (action.payload.id) {
+    try {
+      const response = yield call(
+        instance.delete,
+        `/images/fav/${action.payload.id}`
+      );
 
-    if (response.status === 200) {
-      yield put(setErrorMessage(null));
-      yield put(removeFromFavorites(action.payload));
-    } else {
+      if (response.status === 200) {
+        yield put(setErrorMessage(null));
+        yield put(removeFromFavorites(action.payload));
+      } else {
+        yield put(setErrorMessage("Connectivity problems"));
+      }
+    } catch (error) {
       yield put(setErrorMessage("Connectivity problems"));
+      console.log(error);
     }
-  } catch (error) {
-    yield put(setErrorMessage("Connectivity problems"));
-    console.log(error);
   }
 }
 
